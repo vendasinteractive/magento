449fdba395242081eece6aed9f9a561ddc3a288a
Michael Whalen - Mac <mwhalen08@gmail.com>
2015-02-09 17:30:38 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/code/local/FME/Percentagepricing/controllers/IndexController.php

--- index.php
+++ index.php
@@ -24,6 +24,8 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+Mage::log("Entered index.php", null, "mylogfile.log");
+
 if (version_compare(phpversion(), '5.3.0', '<')===true) {
     echo  '<div style="font:12px/1.35em arial, helvetica, sans-serif;">
 <div style="margin:0 0 25px 0; border-bottom:1px solid #ccc;">
